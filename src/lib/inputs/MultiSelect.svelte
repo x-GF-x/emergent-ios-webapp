a9a7e58afe2c4bd90ba4218a8d32ba1244aead4c
Giancarlo Fiorenzo <gian.fiorenzo@gmail.com>
2023-01-26 10:16:22 -0600

    remove svelte multiselect

--- src/lib/inputs/MultiSelect.svelte
+++ src/lib/inputs/MultiSelect.svelte
@@ -1,15 +1,46 @@
 <script lang="ts">
-	import MultiSelect from 'svelte-multiselect';
-	export let selected: string[] = [];
-	export let options = ['test', 'test2'];
-	export let value: any;
-	export let field: Field = undefined;
-	field = field;
+	import Popper from './generics/Popper.svelte';
+	import SelectHeader from './generics/SelectHeader.svelte';
+	import SelectOption from './generics/SelectOption.svelte';
+	import { fieldOptions } from '$lib/resource_file/lookups/lookups';
 
-	value = value;
+	export let value: string | undefined = undefined;
+	export let props: InputProps = undefined;
+	export let field: Field = undefined;
+
+	console.log(field);
+
+	let fieldLookupId = field?.key;
+	let options = fieldOptions.filter((option) => option.type === fieldLookupId);
+	let popper: Popper;
+	let searchValue = '';
+
+	const selectOption = (code: string) => {
+		if (value === code) {
+			value = undefined;
+		} else {
+			value = code;
+			popper.toggle();
+		}
+	};
 </script>
 
-<MultiSelect bind:selected {options} />
+<Popper
+	bind:this={popper}
+	value={value ? options.find((item) => item.code === value)?.value : ''}
+	{props}
+	toggleIcon={{ open: 'expand_less', closed: 'expand_more' }}
+>
+	<SelectHeader bind:searchValue {field} {props}>
+		<div class="options">
+			{#each options.filter((option) => option.value
+					.toLowerCase()
+					.includes(searchValue.toLowerCase())) as option}
+				<SelectOption {option} {value} on:select={(e) => selectOption(e.detail.code)} />
+			{/each}
+		</div>
+	</SelectHeader>
+</Popper>
 
-<!-- <style>
-</style> -->
+<style>
+</style>
