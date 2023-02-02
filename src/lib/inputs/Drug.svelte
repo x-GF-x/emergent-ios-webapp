8147392fd9015b3b9cc3f5a57e8c7696caf8153a
Giancarlo Fiorenzo <gian.fiorenzo@gmail.com>
2023-02-02 10:37:30 -0600

    build drug component, seperate age functions?

--- src/lib/inputs/Drug.svelte
+++ src/lib/inputs/Drug.svelte
@@ -1,8 +1,46 @@
 <script lang="ts">
-	export let value: string | undefined;
+	import InputBuilder from './generics/InputBuilder.svelte';
+
+	export let value: { [key: string]: FieldValues } = {};
 	export let field: Field = undefined;
-	value = value;
-	field = field;
+
+	let subFields: SubField[];
+
+	if (field?.subFields) {
+		subFields = field.subFields;
+		if (!value) value = {};
+		subFields.forEach((subField) => {
+			if (!value[subField.id]) value[subField.id] = undefined;
+		});
+	}
 </script>
 
-{JSON.stringify(field)}
+{#if Array.isArray(field?.subFields)}
+	<div class="subFields">
+		{#each subFields as subField}
+			{#if value}
+				<div class="subField">
+					<InputBuilder field={subField} bind:value={value[subField.id]} />
+				</div>
+			{/if}
+		{/each}
+	</div>
+{/if}
+
+<style>
+	.subFields {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+	}
+
+	.subField {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.subField:not(:last-child) {
+		border-right: 1px solid var(--border);
+	}
+</style>
