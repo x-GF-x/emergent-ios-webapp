09177c7d410efcc84a81f11db1cd9288a4f2578c
Giancarlo Fiorenzo <gian.fiorenzo@gmail.com>
2023-02-08 17:39:45 -0600

    Move components into dynamic scene

--- src/lib/scenes/DynamicScene.svelte
+++ src/lib/scenes/DynamicScene.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	import { dynamicSceneTypes } from '$lib/data/patient_details';
+	import Actions from './dynamic_scenes/Actions.svelte';
+	import Gallery from './dynamic_scenes/Gallery.svelte';
+	import Notes from './dynamic_scenes/Notes.svelte';
 
 	export let selectedTab: Tab;
 	export let value: DataStorage;
 	export let allCollapsed = false;
 
 	let dynamicIds: DynamicId[] | undefined = selectedTab?.dynamic_ids;
+	const dynamicSceneTypes = { actions: Actions, photo: Gallery, notes: Notes };
 </script>
 
 {#if dynamicIds}
 	{#each dynamicIds as dynamicId}
-		{#if dynamicId !== 'static_fields' && value[dynamicId]}
+		{#if dynamicId !== 'static_fields' && value[dynamicId] && dynamicSceneTypes[dynamicId]}
 			<svelte:component this={dynamicSceneTypes[dynamicId]} {value} {allCollapsed} />
 		{/if}
 	{/each}
